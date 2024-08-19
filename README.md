927f8c40e7c78992195ef9d48081e0ded3c8b543
Hannah Hill <hh.hannahhill@gmail.com>
2024-08-19 08:35:32 +0100

    README updated.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
+This repo demonstrates problem solving katas using recursion. The solutions to the below problems can be found in the recursion.js file. All tests for this can be found in index.test.js.
+
 Problems to solve:
 
 1. Implement a recursive function called reverseStr that reverses a string.
 
 2. Implement a recursive function that sums the digits of a number until only one digit remains.
 
-3. Implement a recursive function that takes a number and returns the number at that point in the Fibonacci sequence. For example, fib(1) would return the first number in the sequence, fib(2) the second, etc.
-
-You are not expected to come up with the Fibonacci formula yourself, just to implement it in JavaScript.
-
-In this problem, the Fibonacci sequence is as follows: 0, 1, 1, 2, 3, 5, 8,...
+3. Implement a recursive function that takes a number and returns the number at that point in the Fibonacci sequence. For example, fib(1) would return the first number in the sequence, fib(2) the second, etc. Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8...
 
 4. Implement a recursive function that totals an arbitrarily nested array of integers.
 
--- recursion.js
+++ recursion.js
@@ -7,97 +7,80 @@ function reverse(string) {
 }
 
 function sumDigits(num) {
-  if(num >= 0 && num <= 9){
-    return num
+  if (num >= 0 && num <= 9) {
+    return num;
   }
-return sumDigits(num % 10 + sumDigits(Math.floor(num / 10)))
- 
+  return sumDigits((num % 10) + sumDigits(Math.floor(num / 10)));
 }
 
 function fib(num) {
-
-  if(num === 1){
+  if (num === 1) {
     return 0;
   }
-  if(num < 4){
+  if (num < 4) {
     return 1;
   }
-  return fib(num - 1)+ fib(num -2);
+  return fib(num - 1) + fib(num - 2);
 }
 
 function deepTotal(arr) {
-  if(arr.length === 0){
+  if (arr.length === 0) {
     return 0;
   }
-  // if arr includes and arr - go to top function?
-  // else do the thing below
 
   let count = 0;
 
-  // base case / stop - arr.length = 1 (nothing to add to it)
-  // add it to count / value to work from
-  if(arr.length === 1){
-    count += arr[0]
+  if (arr.length === 1) {
+    count += arr[0];
   }
-  for(let i = 0; i < arr.length; i++){
-    if(Array.isArray(arr[i])){
-      count += deepTotal(arr[i])
-    }
-    else {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      count += deepTotal(arr[i]);
+    } else {
       count += arr[i];
     }
   }
-  
-  
-  // recursive step / step
-  // self / call to function
-  /*if(!arr.includes([])){
-    return arr.reduce(function (acc, currentValue){
-    return acc + currentValue});
-  }
-  */
 
-  // return count!
   return count;
 }
 
 function deepIncludes(arr, val) {
-
-  if(arr.length === 0){
+  if (arr.length === 0) {
     return false;
   }
-  
-  for(let i = 0; i < arr.length; i++){
-    const item = arr[i]
-    if(item === val){
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (item === val) {
       return true;
+    } else if (Array.isArray(arr[i])) {
+      if (deepIncludes(arr[i], val)) {
+        return true;
+      }
     }
-    else if(Array.isArray(arr[i])){
-        if(deepIncludes(arr[i], val)){
-          return true;
-        }
-      }  
-    }
-    return false;  
   }
+  return false;
+}
 
-  function countObjects(obj) {
+function countObjects(obj) {
+  let objCount = 1;
 
-   let objCount = 1
-
-    for(const item in obj){
-      if(typeof obj[item] === "object"){
-        objCount+= countObjects(obj[item])
-      }
-      if(typeof obj[item] !== "object"){
-        continue;
-      }
+  for (const item in obj) {
+    if (typeof obj[item] === "object") {
+      objCount += countObjects(obj[item]);
     }
+    if (typeof obj[item] !== "object") {
+      continue;
+    }
+  }
   return objCount;
-  }
-  // base case is object with no object as item
+}
 
-
-
-
-module.exports = {reverse,sumDigits, fib, deepTotal, deepIncludes, countObjects,}
+module.exports = {
+  reverse,
+  sumDigits,
+  fib,
+  deepTotal,
+  deepIncludes,
+  countObjects,
+};
